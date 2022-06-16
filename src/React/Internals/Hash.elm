407a7fb7dcb4fe00a6def425fa36df7a7070dfed
Christian <2013206+kress95@users.noreply.github.com>
2022-06-16 18:14:01 -0300

    Refactor React.Internals.Hash

--- src/React/Internals/Hash.elm
+++ src/React/Internals/Hash.elm
@@ -1,74 +1,185 @@
 module React.Internals.Hash exposing
     ( Hash, initialSeed
-    , fromInt, fromIntWithSeed
-    , fromString, fromStringWithSeed
-    , combineAsymmetric, combineSymmetric
+    , join, combine
+    , string, stringWith
+    , int, intWith
+    , longerInt, longerIntWith
+    , anyInt, anyIntWith
+    , float, floatWith
+    , anyFloat, anyFloatWith
     )
 
-{-| Mostly copied from <https://github.com/robinheghan/fnv1a>
+{-| FNV1a but with a encoder-like API.
+
+
+# Basics
 
 @docs Hash, initialSeed
-@docs fromInt, fromIntWithSeed
-@docs fromString, fromStringWithSeed
-@docs combineAsymmetric, combineSymmetric
+@docs join, combine
+
+
+# String
+
+@docs string, stringWith
+
+
+# Int
+
+@docs int, intWith
+@docs longerInt, longerIntWith
+@docs anyInt, anyIntWith
+
+
+# Float
+
+@docs float, floatWith
+@docs anyFloat, anyFloatWith
 
 -}
 
 import Bitwise as Bit
+import FNV1a
 
 
 type alias Hash =
     Int
 
 
-fromInt : Int -> Hash
-fromInt n =
-    fromIntWithSeed n initialSeed
-
-
-fromIntWithSeed : Int -> Hash -> Hash
-fromIntWithSeed n seed =
-    -- only well behaved with 32bit integers
-    hash (Bit.and n 0xFF) seed
-        |> hash (Bit.and (Bit.shiftRightBy n 8) 0xFF)
-        |> hash (Bit.and (Bit.shiftRightBy n 16) 0xFF)
-        |> hash (Bit.and (Bit.shiftRightBy n 24) 0xFF)
-
-
-combineAsymmetric : Int -> Hash -> Hash
-combineAsymmetric otherSeed seed =
-    -- not sure how worse this is compared to the function above
-    Bit.xor (otherSeed * 16777619) seed
-
-
-combineSymmetric : Int -> Hash -> Hash
-combineSymmetric otherSeed seed =
-    -- not sure how worse this is compared to the function above
-    Bit.xor otherSeed seed * 16777619
-
-
-fromString : String -> Hash
-fromString string =
-    fromStringWithSeed string initialSeed
-
-
-fromStringWithSeed : String -> Hash -> Hash
-fromStringWithSeed str seed =
-    Bit.shiftRightZfBy 0 (String.foldl utf32ToUtf8 seed str)
-
-
 initialSeed : Hash
 initialSeed =
-    0x811C9DC5
+    FNV1a.initialSeed
+
+
+join : Hash -> Hash -> Hash
+join a seed =
+    -- not sure how worse this is compared to the function above
+    Bit.xor (a * 16777619) seed
+
+
+combine : Hash -> Hash -> Hash
+combine a seed =
+    -- not sure how worse this is compared to the function above
+    Bit.xor a seed * 16777619
 
 
 
--- internals
+-- String
 
 
-hash : Int -> Int -> Int
-hash byte hashValue =
-    {- Implementation ported from: https://gist.github.com/vaiorabbit/5657561 -}
+string : String -> Hash
+string a =
+    stringWith a initialSeed
+
+
+stringWith : String -> Hash -> Hash
+stringWith a seed =
+    FNV1a.hashWithSeed a seed
+
+
+
+-- Int
+
+
+int : Int -> Hash
+int a =
+    intWith a initialSeed
+
+
+intWith : Int -> Hash -> Hash
+intWith a seed =
+    -- only well behaved with 32bit integers
+    hasher (Bit.and a 0xFF) seed
+        |> hasher (Bit.and (Bit.shiftRightBy a 8) 0xFF)
+        |> hasher (Bit.and (Bit.shiftRightBy a 16) 0xFF)
+        |> hasher (Bit.and (Bit.shiftRightBy a 24) 0xFF)
+
+
+
+-- Longer Int
+
+
+longerInt : Int -> Hash
+longerInt a =
+    longerIntWith a initialSeed
+
+
+longerIntWith : Int -> Hash -> Hash
+longerIntWith a seed =
+    -- slower but range is (Number.MAX_SAFE_INTEGER + 1) / 2
+    let
+        --  a + ((Number.MAX_SAFE_INTEGER + 1) / 2)
+        x =
+            abs (a + 4503599627370496)
+
+        y =
+            x // 2 ^ 32
+    in
+    hasher (shiftRightZfBy24 y) seed
+        |> hasher (shiftRightZfBy24 (Bit.shiftLeftBy y 8))
+        |> hasher (shiftRightZfBy24 (Bit.shiftLeftBy y 16))
+        |> hasher (shiftRightZfBy24 (Bit.shiftLeftBy y 24))
+        |> hasher (shiftRightZfBy24 x)
+        |> hasher (shiftRightZfBy24 (Bit.shiftLeftBy x 8))
+        |> hasher (shiftRightZfBy24 (Bit.shiftLeftBy x 16))
+        |> hasher (shiftRightZfBy24 (Bit.shiftLeftBy x 24))
+
+
+
+-- Any Int
+
+
+anyInt : Int -> Hash
+anyInt a =
+    anyIntWith a initialSeed
+
+
+anyIntWith : Int -> Hash -> Hash
+anyIntWith a seed =
+    stringWith (String.fromInt a) seed
+
+
+
+-- Float
+
+
+float : Float -> Hash
+float a =
+    floatWith a initialSeed
+
+
+floatWith : Float -> Hash -> Hash
+floatWith a seed =
+    -- faster but range is ((Number.MAX_SAFE_INTEGER + 1) / 2) / 100000
+    longerIntWith (round (a ^ 100000)) seed
+
+
+
+-- Any Float
+
+
+anyFloat : Float -> Hash
+anyFloat a =
+    anyFloatWith a initialSeed
+
+
+anyFloatWith : Float -> Hash -> Hash
+anyFloatWith a seed =
+    stringWith (String.fromFloat a) seed
+
+
+
+-- Internals
+
+
+shiftRightZfBy24 : Int -> Int
+shiftRightZfBy24 x =
+    Bit.shiftRightZfBy x 24
+
+
+hasher : Int -> Int -> Int
+hasher byte hashValue =
+    -- copied from: https://github.com/robinheghan/fnv1a
+    -- (implementation ported from: https://gist.github.com/vaiorabbit/5657561)
     let
         mixed =
             Bit.xor byte hashValue
@@ -79,32 +190,3 @@ hash byte hashValue =
         + Bit.shiftLeftBy 7 mixed
         + Bit.shiftLeftBy 8 mixed
         + Bit.shiftLeftBy 24 mixed
-
-
-utf32ToUtf8 : Char -> Int -> Int
-utf32ToUtf8 char seed =
-    {- Implementation copied from: https://github.com/zwilias/elm-utf-tools/tree/2.0.1 -}
-    let
-        byte =
-            Char.toCode char
-    in
-    if byte < 0x80 then
-        hash byte seed
-
-    else if byte < 0x0800 then
-        seed
-            |> hash (Bit.or 0xC0 <| Bit.shiftRightZfBy 6 byte)
-            |> hash (Bit.or 0x80 <| Bit.and 0x3F byte)
-
-    else if byte < 0x00010000 then
-        seed
-            |> hash (Bit.or 0xE0 <| Bit.shiftRightZfBy 12 byte)
-            |> hash (Bit.or 0x80 <| Bit.and 0x3F <| Bit.shiftRightZfBy 6 byte)
-            |> hash (Bit.or 0x80 <| Bit.and 0x3F byte)
-
-    else
-        seed
-            |> hash (Bit.or 0xF0 <| Bit.shiftRightZfBy 18 byte)
-            |> hash (Bit.or 0x80 <| Bit.and 0x3F <| Bit.shiftRightZfBy 12 byte)
-            |> hash (Bit.or 0x80 <| Bit.and 0x3F <| Bit.shiftRightZfBy 6 byte)
-            |> hash (Bit.or 0x80 <| Bit.and 0x3F byte)
