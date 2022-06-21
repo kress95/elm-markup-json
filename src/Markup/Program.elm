1e6cc407e3effb7480e57ec9de0ed94d3381d8e1
Christian <2013206+kress95@users.noreply.github.com>
2022-06-21 02:49:54 -0300

    Remove last react ref

--- src/Markup/Program.elm
+++ src/Markup/Program.elm
@@ -1,38 +1,42 @@
-module React.Program exposing (Program, ProgramCmd, ProgramMsg, ProgramSub, worker)
+module Markup.Program exposing (PortsCmd, PortsMsg, PortsSub, Program, program)
 
 import Json.Decode as Decode exposing (Decoder, Value)
+import Markup exposing (Markup)
 import Platform
-import React.Html as Html exposing (Html)
-import React.VirtualDom as VirtualDom
 
 
-type ProgramModel model
-    = Model Html model
+type PortsModel model
+    = Model Markup model
 
 
-type ProgramMsg msg
+type PortsMsg msg
     = Msg msg
     | EventError Decode.Error
 
 
-type alias ProgramCmd msg =
-    Cmd (ProgramMsg msg)
+type alias PortsCmd msg =
+    Cmd (PortsMsg msg)
 
 
-type alias ProgramSub msg =
-    Sub (ProgramMsg msg)
-
-
-type alias Config flags model msg =
-    Init (Update (Subscriptions {} model msg) model msg) flags model msg
+type alias PortsSub msg =
+    Sub (PortsMsg msg)
 
 
 type alias Program flags model msg =
-    Platform.Program flags (ProgramModel model) (ProgramMsg msg)
+    Platform.Program flags (PortsModel model) (PortsMsg msg)
 
 
-worker : Config flags model msg -> Program flags model msg
-worker config =
+program :
+    { init : flags -> ( model, Cmd msg )
+    , view : model -> Markup
+    , update : msg -> model -> ( model, Cmd msg )
+    , subscriptions : model -> Sub msg
+    , send : Value -> PortsCmd msg
+    , receive : (Value -> PortsMsg msg) -> PortsSub msg
+    , expect : Decoder msg
+    }
+    -> Program flags model msg
+program config =
     Platform.worker
         { init = initFrom config
         , update = updateFrom config
@@ -47,12 +51,12 @@ worker config =
 type alias Init a flags model msg =
     { a
         | init : flags -> ( model, Cmd msg )
-        , view : model -> Html
-        , send : Value -> ProgramCmd msg
+        , view : model -> Markup
+        , send : Value -> PortsCmd msg
     }
 
 
-initFrom : Init a flags model msg -> flags -> ( ProgramModel model, ProgramCmd msg )
+initFrom : Init a flags model msg -> flags -> ( PortsModel model, PortsCmd msg )
 initFrom { init, view, send } flags =
     let
         ( model, cmd ) =
@@ -64,7 +68,7 @@ initFrom { init, view, send } flags =
     ( Model html model
     , Cmd.batch
         [ Cmd.map Msg cmd
-        , send (Html.encode html)
+        , send (Markup.encode html)
         ]
     )
 
@@ -72,12 +76,12 @@ initFrom { init, view, send } flags =
 type alias Update a model msg =
     { a
         | update : msg -> model -> ( model, Cmd msg )
-        , view : model -> Html
-        , send : Value -> ProgramCmd msg
+        , view : model -> Markup
+        , send : Value -> PortsCmd msg
     }
 
 
-updateFrom : Update a model msg -> ProgramMsg msg -> ProgramModel model -> ( ProgramModel model, ProgramCmd msg )
+updateFrom : Update a model msg -> PortsMsg msg -> PortsModel model -> ( PortsModel model, PortsCmd msg )
 updateFrom { update, view, send } msg (Model html model) =
     let
         ( model_, cmd ) =
@@ -89,17 +93,18 @@ updateFrom { update, view, send } msg (Model html model) =
                     -- TODO: create error page
                     ( model, Cmd.none )
 
+        -- TODO only update on request animation frame
         html_ =
             view model_
     in
     ( Model html_ model_
-    , if VirtualDom.isEqual html html_ then
+    , if Markup.isEqual html html_ then
         Cmd.map Msg cmd
 
       else
         Cmd.batch
             [ Cmd.map Msg cmd
-            , send (VirtualDom.encode html_)
+            , send (Markup.encode html_)
             ]
     )
 
@@ -107,12 +112,12 @@ updateFrom { update, view, send } msg (Model html model) =
 type alias Subscriptions a model msg =
     { a
         | subscriptions : model -> Sub msg
-        , receive : (Value -> ProgramMsg msg) -> ProgramSub msg
+        , receive : (Value -> PortsMsg msg) -> PortsSub msg
         , expect : Decoder msg
     }
 
 
-subscriptionsFrom : Subscriptions a model msg -> ProgramModel model -> ProgramSub msg
+subscriptionsFrom : Subscriptions a model msg -> PortsModel model -> PortsSub msg
 subscriptionsFrom { subscriptions, receive, expect } (Model _ model) =
     Sub.batch
         [ Sub.map Msg (subscriptions model)
@@ -120,7 +125,7 @@ subscriptionsFrom { subscriptions, receive, expect } (Model _ model) =
         ]
 
 
-subscriptionsDecode : Decoder msg -> Value -> ProgramMsg msg
+subscriptionsDecode : Decoder msg -> Value -> PortsMsg msg
 subscriptionsDecode expect value =
     case Decode.decodeValue expect value of
         Ok msg ->
